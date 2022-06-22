db808b6d2db80680c97813933b45c9d76c60f598
BetaLixT <39497438+BetaLixT@users.noreply.github.com>
2022-06-22 17:51:57 +0400

    minor fixes for default values

--- Models/EventEntity.cs
+++ Models/EventEntity.cs
@@ -2,12 +2,12 @@ namespace BetaLixt.StreamNotification.Models;
 
 public class EventEntity
 {
-    public string ServiceName { get; set; }
-    public string Id { get; set; }
-    public string Stream { get; set; }
-    public string StreamId { get; set; }
+    public string ServiceName { get; set; } = "";
+    public string Id { get; set; } = "";
+    public string Stream { get; set; } = "";
+    public string StreamId { get; set; } = "";
     public long StreamVersion { get; set; } = -1;
-    public string Event { get; set; }
-    public object? Data { get; set; }
+    public string Event { get; set; } = "";
+    public object? Data { get; set; } = "";
     public DateTimeOffset CreatedDateTime { get; set; }
 }
